f89ad87fbb1d9eb2755dd3afa6f5e67122136265
charleygros <charley.gros@gmail.com>
2021-08-23 14:02:12 +1000

    resize order

--- code/loader.py
+++ code/loader.py
@@ -24,10 +24,8 @@ class WaldoLoader(Dataset):
         self.list_gt = [load_image(path_gt, as_grayscale=True) for path_gt in self.list_path_gt]
         print("Resizing image dimensions so that they are a multiple of the patch size: {} x {} pixels^2 ...".format(self.size_patch, self.size_patch))
         self.list_img = [resize_image(img, size_patch=self.size_patch) for img in self.list_img]
-        self.list_gt = [resize_image(gt, size_patch=self.size_patch) for gt in self.list_gt]
-
-        # Binary values for GTs
         self.list_gt = [gt / 255 for gt in self.list_gt]
+        self.list_gt = [resize_image(gt.astype(bool), size_patch=self.size_patch) for gt in self.list_gt]
 
         # Patch extraction
         self.list_patch_img, self.list_patch_gt = extract_all_patches(self.list_img, self.list_gt, self.size_patch)
