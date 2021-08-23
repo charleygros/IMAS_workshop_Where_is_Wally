4ca8f05b971eb1385022ca0a3a97f2376e2daa7b
charleygros <charley.gros@gmail.com>
2021-08-23 14:32:46 +1000

    arr

--- code/loader.py
+++ code/loader.py
@@ -83,6 +83,7 @@ def extract_positive_patch(img, coords_bbox, size_patch):
     half_size_patch = size_patch // 2
     i = img[w_middle-half_size_patch:w_middle+half_size_patch,
         h_middle-half_size_patch:h_middle+half_size_patch]
+    #i = img[wend - 224:wstart + 224, hend - 224:hstart + 224]
     if i.shape[0] != size_patch or i.shape[1] != size_patch:
         print(i.shape, w_middle, h_middle, w_start, w_end, h_start, h_end)
         i = img[w_start:w_start+size_patch+1, h_start:h_start+size_patch+1]
